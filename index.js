6815e4b799df5c5219dd85cb6056d48f625ce357
Ahmad Guenoun <amguenoun@gmail.com>
2019-11-08 17:37:29 -0500

    set up server to listen to port

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+const port = 8000;
+
+server.use(express.json());
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
