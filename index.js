587106e2db976c441df576a426fcce864f523828
Ahmad Guenoun <amguenoun@gmail.com>
2019-11-08 17:46:34 -0500

    created get request

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db');
+
 const server = express();
 
 const port = 8000;
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then((data) => res.json(data))
+        .catch((err) => res.json("Error on db"))
+});
 
 server.listen(port, () => {
     console.log(`Listening on port ${port}`);
